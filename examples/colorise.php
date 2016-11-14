9ae160269ec829cb26347e88847cb87732ce7ebe
twelvee <redapple54r@gmail.com>
2016-11-14 21:51:59 +0700

    Upload files

---
+++ examples/colorise.php
@@ -0,0 +1,11 @@
+<?php
+require_once("../classes/Filter.php");
+
+$image = imagecreatefromjpeg(isset($argv[1]) ? $argv[1] : "example.jpg");
+$filter= new Filter($image);
+
+$effects = $filter->colorise();
+
+header("Content-type: image/png");
+imagepng($image);
+imagedestroy($image);
